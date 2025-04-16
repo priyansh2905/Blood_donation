91f2465172fb0e51be934ab3f243f2cfc7165bd9
purabagr2406 <purabagrawal6@gmail.com>
2025-04-16 22:44:00 +0530

    added email services for reminder and donor details.

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -4,8 +4,18 @@ const app = express();
 const dotenv = require("dotenv");
 const cron = require("node-cron");
 const dbConnection = require("./utils/db");
-const { sendDetailsProspectEmail } = require("./EmailServices/sendDetailsProspect");
-const { sendEligibilityEmail } = require("./EmailServices/sendEligibilityEmail");
+const {
+  sendDetailsProspectEmail,
+} = require("./EmailServices/sendDetailsProspect");
+const {
+  sendEligibilityEmail,
+} = require("./EmailServices/sendEligibilityEmail");
+const {
+  sendBloodDonationReminder,
+} = require("./EmailServices/sendBloodDonationReminder");
+const {
+  sendDonorDetailsEmail,
+} = require("./EmailServices/sendDonorDetailsEmail");
 dotenv.config();
 
 //Server
@@ -21,5 +31,7 @@ const run = () => {
   cron.schedule("* * * * * *", () => {
     sendDetailsProspectEmail();
     sendEligibilityEmail();
+    sendBloodDonationReminder();
+    sendDonorDetailsEmail();
   });
 };
